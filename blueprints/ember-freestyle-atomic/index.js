3cef3434eb5c60539990941791d6f669735d0a18
Todd Gandee <tgandee@bignerdranch.com>
2016-08-03 13:53:01 -0400

    update main blueprint

--- blueprints/ember-freestyle-atomic/index.js
+++ blueprints/ember-freestyle-atomic/index.js
@@ -1,15 +1,51 @@
 /*jshint node:true*/
+
+var Blueprint  = require('../../lib/models/blueprint');
+
 module.exports = {
   name: 'ember-freestyle-atomic',
 
   normalizeEntityName: function() {},
 
   afterInstall: function() {
+    var modelOptions = merge({}, options, {
+      entity: {
+        name: 'styleguide'
+      }
+    });
+
+    var mainBlueprint = Blueprint.lookup("route", {
+      ui: this.ui,
+      analytics: this.analytics,
+      project: this.project
+    });
+
     var bowerPackages = [
       { name: 'remarkable', target: '1.6.2' },
       { name: 'highlightjs', target: '9.1.0' }
     ];
 
-    return this.addBowerPackagesToProject(bowerPackages);
+    return Promise.resolve()
+      .then(function() {
+        return mainBlueprint["install"](modelOptions);
+      })
+      .then(function() {
+        var testBlueprint = mainBlueprint.lookupBlueprint(name + '-test', {
+          ui: this.ui,
+          analytics: this.analytics,
+          project: this.project,
+          ignoreMissing: true
+        });
+
+        if (!testBlueprint) { return; }
+
+        if (testBlueprint.locals === Blueprint.prototype.locals) {
+          testBlueprint.locals = function(options) {
+            return mainBlueprint.locals(options);
+          };
+        }
+
+        return this.addBowerPackagesToProject(bowerPackages);
+      });
   }
 };
