fe7f1a4edd9528e7777a24836ca31b480cd630db
Todd Gandee <tgandee@bignerdranch.com>
2016-08-16 10:27:54 -0400

    update blueprint

--- blueprints/ember-freestyle-atomic/index.js
+++ blueprints/ember-freestyle-atomic/index.js
@@ -13,7 +13,16 @@ module.exports = {
   normalizeEntityName: function(){
     return "styleguide";
   },
+  // afterInstall: function(options) {
+  //   return this.lookupBlueprint('route').install(options);
+  // },
   afterInstall: function(options) {
-    return this.lookupBlueprint('route').install(options);
-  }
+   var entityName = options.entity.name;
+   var routerStr = '\n  this.route("' + entityName + '");'
+
+   return this.insertIntoFile('app/router.js', routerStr, {
+       after: 'Route.map(Router, function() {'
+     });
+   });
+ }
 };
